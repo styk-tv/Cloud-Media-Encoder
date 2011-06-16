a072a441c9ac8ad333560535b67e25671baa7ae3
Jakub Stachowski <qbast@go2.pl>
2011-06-16 15:51:52 +0200

    Improve getprogress

--- src/node/getprogress.py
+++ src/node/getprogress.py
@@ -1,32 +1,18 @@
-from xml.dom.minidom import getDOMImplementation, parse
-import os
+from nodetools.xmlqueue import XMLJobManager
 from nodetools.tools import xmlmsg
 import sys
 
 
 def main():
-  defaultdir="encroot"
-  if len(sys.argv)>1: defaultdir=sys.argv[1]
-  if not os.path.exists(defaultdir+"/status") or not os.path.exists(defaultdir+"/queue"):  return xmlmsg("error","Wrong directory "+defaultdir)
-
-  waiting=len(os.listdir(defaultdir+"/queue"))-2
-  current=None
-
-  cfile=defaultdir+"/status/current.xml"
-  if os.path.exists(cfile):
-    current_doc=parse(open(cfile,'r'))
-    curid=os.readlink(cfile)
-    current=current_doc.documentElement
-    current.tagName="current"
-
-  impl=getDOMImplementation()
-  doc=impl.createDocument(None, "queue", None)
-  doc.documentElement.setAttribute("waiting",str(waiting))
-  if current<>None:
-    current.setAttribute("id",str(curid))
-    doc.documentElement.appendChild(current)
-
-  doc.writexml(sys.stdout)
+  try:
+    jman=XMLJobManager()
+    doc=jman.load()
+    for wfnode in doc.getElementsByTagName("workflow"):
+        while wfnode.firstChild<>None: wfnode.removeChild(wfnode.firstChild)
+    doc.writexml(sys.stdout)
+    
+  except Exception,  e:
+      return xmlmsg("error", str(e))
   
 if __name__=="__main__":
   main()
