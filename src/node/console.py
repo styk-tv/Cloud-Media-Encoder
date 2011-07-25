37a59c95f8677e09a11e50ab7e4ee1b776413510
Jakub Stachowski <qbast@go2.pl>
2011-07-25 20:51:52 +0200

    Console

---
+++ src/node/console.py
@@ -0,0 +1,100 @@
+#!/usr/bin/python
+
+##
+## This file is part of the Styk.TV API project.
+##
+## Copyright (c) 2011 Piotr Styk (peter@styk.tv)
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License version 2 
+## as published by  the Free Software Foundation
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+##
+
+from nodetools import version
+from nodetools import pwdtools
+from nodetools.config import Config
+import sys
+
+def main_menu():
+    print "(c) 2011 Node Styk.Tv v0.2 ", version.commit, " (", version.date+")"
+    print "Node ID:",  version.nodeid
+    print "IP: ", pwdtools.getMainIp(), "  (", pwdtools.getIfaceType()+")"
+    print
+    print "QUEUE: "
+    print "WORKFLOWS"
+    print
+    print "1) Network setting"
+    print "2) Change root password"
+    print "3) Change node password"
+    print "4) Exit"
+    
+    return sys.stdin.readline().strip()
+    
+def network_menu():
+    print "Network settings"
+    print
+    print "1) Use DHCP"
+    print "2) Use static configuration"
+    print "3) Exit"
+    return sys.stdin.readline().strip()
+
+def readStaticSettings():
+    print "IP Address:"
+    ip=sys.stdin.readline().strip()
+    print "Netmask (xxx.xxx.xxx.xxx):"
+    netmask=sys.stdin.readline().strip()
+    print "Gateway"
+    gateway=sys.stdin.readline().strip()
+    print 'First DNS server:'
+    dns1=sys.stdin.readline().strip()
+    print 'Second DNS server:'
+    dns2=sys.stdin.readline().strip()
+    return (ip, netmask, gateway, dns1, dns2)
+
+def network_settings():
+    while True:
+        opt=network_menu()
+        if opt=="1":
+            pwdtools.setDhcp()
+            pwdtools.restartNetwork()
+        elif opt=="2":
+            (ip, netmask, gateway, dns1, dns2)=readStaticSettings()
+            pwdtools.setStatic(ip, gateway, netmask, dns1, dns2)
+            pwdtools.restartNetwork()
+        elif opt=="3":
+            return
+    
+def root_pwd():
+    try:
+        pwdtools.change_password("root")
+    except Exception, e:
+        print e
+
+def node_pwd():
+    try:
+        pwdtools.change_password(Config.USER)
+    except Exception, e:
+        print e
+
+
+def main():
+    while True:
+        opt=main_menu()
+        if opt=="1": network_settings()
+        elif opt=="2": root_pwd()
+        elif opt=="3": node_pwd()
+        elif opt=="4": return
+        
+
+if __name__=="__main__":
+    main()
+    
---
+++ src/node/nodetools/pwdtools.py
@@ -0,0 +1,73 @@
+##
+## This file is part of the Styk.TV API project.
+##
+## Copyright (c) 2011 Piotr Styk (peter@styk.tv)
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License version 2 
+## as published by  the Free Software Foundation
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+##
+
+import PAM
+import sys
+import subprocess
+from getpass import getpass
+import socket
+
+MAIN_IFACE="eth0"
+
+def check_password(user):
+    auth=PAM.pam()
+    auth.start("passwd")
+    auth.set_item(PAM.PAM_USER,user)
+    print "Enter OLD password"
+    auth.authenticate()
+
+
+def change_password(user):
+    check_password(user)
+    np=getpass("Enter NEW password: ")
+    np2=getpass("Repeat NEW password: ")
+    if np<>np2: raise Exception("Password does not match")
+    passwd=subprocess.Popen(args=["passwd", "--stdin", user], stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+    ret=passwd.communicate(np)
+    passwd.stdin.close()
+    if passwd.wait() != 0: raise Exception("Error changing password")
+    
+
+def getMainIp():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("gmail.com",80))
+    return s.getsockname()[0]
+
+def getIfaceType():
+    with open("/etc/network/interfaces","r") as f:
+        for l in f:
+            tokens=l.strip().split(" ")
+            if tokens[0]=="iface" and tokens[1]==MAIN_IFACE and tokens[2]=="inet":  return tokens[3]
+    return "unknown"
+    
+def setDhcp():
+        with open("/etc/network/interfaces","w") as f:
+            f.write("auto lo\niface lo inet loopback\n\nallow-hotplug "+MAIN_IFACE+"\niface "+MAIN_IFACE+" inet dhcp\n")
+
+def setStatic(ip, gw, netmask, dns1, dns2):
+        with open("/etc/network/interfaces","w") as f:
+            f.write("auto lo\niface lo inet loopback\n\nallow-hotplug "+MAIN_IFACE+"\niface "+MAIN_IFACE+" inet static\n")
+            f.write("   address "+ip+"\n")
+            f.write("   netmask "+netmask+"\n")
+            f.write("   gateway "+gw+"\n")
+        with open("/etc/resolv.conf","w") as f:
+            f.write("nameserver "+dns1+"\nnameserver "+dns2+"\n")
+
+def restartNetwork():
+        subprocess.call("/etc/init.d/network restart", shell=True)
