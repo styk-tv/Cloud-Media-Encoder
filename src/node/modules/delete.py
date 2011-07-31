683ecfabafbb0b9f291182c26d4d8ff8d825e9c3
Jakub Stachowski <qbast@go2.pl>
2011-07-31 18:32:11 +0200

    DELETE action

---
+++ src/node/modules/delete.py
@@ -0,0 +1,39 @@
+##
+## This file is part of the Styk.TV API project.
+##
+## Copyright (c) 2011 Piotr Styk (peter@styk.tv)
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License version 2 
+## as published by  the Free Software Foundation
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+##
+
+
+from nodetools.xmlqueue import XMLJobManager
+from nodetools.abstractqueue import AbstractTaskExecutor,Queue
+from nodetools.localstores import LocalStoreList
+from nodetools.tools import tools
+from shutil import rmtree
+
+
+class DeleteExecutor(AbstractTaskExecutor):
+    def __init__(self,reporter, workflow,task):
+        super(DeleteExecutor, self).__init__(reporter, workflow, task)
+        slist=LocalStoreList()
+        self.srcasset=slist.getByUuid(task.attributes["srcStore"]).findAsset(task.attributes["srcAssetItem"])
+        
+    def run(self):
+        rmtree(self.srcasset)
+   
+
+def pluginInfo():
+    return "DELETE", DeleteExecutor
